ff1bba0a2274c799f7c13e6976b42e2bd830f23d
Devedse <davydavidse@gmail.com>
2023-09-02 16:30:48 +0200

    Cleanup

--- DeveOspStreamCombiner/Program.cs
+++ DeveOspStreamCombiner/Program.cs
@@ -13,7 +13,6 @@ namespace DeveOspStreamCombiner
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             builder.Services.AddHttpClient();
 
